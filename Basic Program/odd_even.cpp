26363ea7c029f78f7045fe26e6b0dd29e323a9c5
nitin25ch8-sketch <nitin25ch8@gmail.com>
2026-02-07 18:45:09 +0530

    Create odd_even.cpp

---
+++ Basic Program/odd_even.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter your number"<<endl;
+    cin>>a;
+    if ( a%2==0){
+        cout<<"Number is even"<<endl;
+    }
+    else{
+        cout<<"Number is odd";
+    }
+ getch();
+ return 0;
+}
