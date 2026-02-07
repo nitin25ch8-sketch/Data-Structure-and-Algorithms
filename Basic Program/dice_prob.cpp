1991cb24f16b7a8b2dc2f62276c7174b2f6d369e
nitin25ch8-sketch <nitin25ch8@gmail.com>
2026-02-07 18:44:57 +0530

    Create dice_prob.cpp

---
+++ Basic Program/dice_prob.cpp
@@ -0,0 +1,25 @@
+1#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter your number"<<endl;
+    cin>>a;
+    if(a==1){
+        cout<<6<<endl;
+    }
+    if(a==2){
+        cout<<5<<endl;
+    }if(a==6){
+        cout<<1<<endl;
+    }if(a==5){
+        cout<<2<<endl;
+    }if(a==3){
+        cout<<4<<endl;
+    }
+    if(a==4){
+        cout<<3<<endl;
+    }
+ getch();
+ return 0;
+}
