515f22b3984f584365993042dbcb63378dde9b7f
nitin25ch8-sketch <nitin25ch8@gmail.com>
2026-02-09 19:08:39 +0530

    Create First.cpp

---
+++ Arrays/Basics/First.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<conio.h>
+#include<algorithm>
+using namespace std;
+int main(){
+    // Initialization of an Array
+   int arr[5]={1,2,3,4,5}; // Initializes all 5 elements
+   int arr2[]= {1,2,3,4,5,6}; // Array size is automatically set to 5
+return 0;
+}
