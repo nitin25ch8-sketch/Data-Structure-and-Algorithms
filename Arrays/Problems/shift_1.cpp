6c17e23348970382b16953138b017b72e74fbb45
nitin25ch8-sketch <nitin25ch8@gmail.com>
2026-04-08 05:25:05 +0530

    Add files via upload

---
+++ Arrays/Problems/shift_1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+    int arr[]={10,20,30,40,50};
+    int n=5;
+    int temp=arr[n-1];
+    for(int i=n-1; i>=1;i--){
+        arr[i]=arr[i-1];
+    };
+    arr[0]=temp;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+ return 0;
+}
